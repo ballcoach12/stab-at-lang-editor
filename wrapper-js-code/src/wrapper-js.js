1786e6fdb82d7b0debb0fc41ec882168240851a0
Greg Lusk <gregory_lusk@selinc.com>
2023-09-19 09:49:54 -0400

    Corrected error in buildWorkerDefinition path

--- wrapper-js-code/src/wrapper-js.js
+++ wrapper-js-code/src/wrapper-js.js
@@ -1,20 +1,19 @@
 import { startEditor } from './common.js';
-
 import 'monaco-editor/esm/vs/basic-languages/typescript/typescript.contribution.js';
 import 'monaco-editor/esm/vs/language/typescript/monaco.contribution.js';
-
 import { buildWorkerDefinition } from 'monaco-editor-workers';
-import { UserConfig } from 'monaco-editor-wrapper';
-
-buildWorkerDefinition('../node_modules/monaco-editor-workers/dist/workers', import.meta.url, false);
-
+buildWorkerDefinition('./node_modules/monaco-editor-workers/dist/workers', import.meta.url, false);
+//import { MonacoEditorLanguageClientWrapper } from 'monaco-editor-wrapper';
+//import { addMonacoStyles, MonacoEditorLanguageClientWrapper } from '../libs/monaco-editor-wrapper/mew.js';
+//addMonacoStyles('monaco-styles-helper');
 const codeUri = '/tmp/hello.ts';
 let code = `function sayHello(): string {
     return "Hello";
 };`;
-
-
-
+// const codeOriginalUri = '/tmp/goodbye.ts';
+// let codeOriginal = `function sayGoodbye(): string {
+//     return "Goodbye";
+// };`;
 const monacoEditorConfig = {
     glyphMargin: true,
     guides: {
@@ -26,9 +25,8 @@ const monacoEditorConfig = {
     theme: 'vs-dark',
     renderSideBySide: false
 };
-
-const userConfig: UserConfig = {
-    htmlElement: document.getElementById('monaco-editor-root') as HTMLElement,
+const userConfig = {
+    htmlElement: document.getElementById('monaco-editor-root'),
     wrapperConfig: {
         serviceConfig: {
             enableKeybindingsService: true,
@@ -45,11 +43,10 @@ const userConfig: UserConfig = {
         }
     }
 };
-
-
-
-try{
-  startEditor(userConfig, code);
-} catch (e) {
+try {
+    startEditor(userConfig, code);
+}
+catch (e) {
     console.error(e);
 }
+//# sourceMappingURL=wrapper-js.js.map
